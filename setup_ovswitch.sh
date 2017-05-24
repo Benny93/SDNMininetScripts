76b3d27af4e51869af068faa819402cd2e7200cb
Benny93 <b.vollmer@student.uni-tuebingen.com>
2017-05-24 10:37:57 +0200

    setup ovswitch improved

--- setup_ovswitch.sh
+++ setup_ovswitch.sh
@@ -1,21 +1,31 @@
 /etc/init.d/openvswitch-switch restart
-IF='eth1'
+#IF='eth1'
+IF='enp0s8'
 IP=2017:db8::f101/120
 GW=2017:db8::f1ff
 IP_CTLR=2017:db8::ffaa
 
 #add bridge
+echo "ovs-vsctl add-br br-int"
 ovs-vsctl add-br br-int
+echo "ovs-vsctl add-port br-int $IF"
 ovs-vsctl add-port br-int $IF
+echo "ifconfig $IF 0"
 ifconfig $IF 0
 #Zero out your eth0 interface and slap it on the bridge interface
 #(warning will clip you unless you script it)
 #ifconfig br-int 192.168.1.208 netmask 255.255.255.0
+echo "ip -6 a a $IP dev br-int"
 ip -6 a a $IP dev br-int
 #route add default gw 192.168.1.1 br-int
+echo "ip -6 r a default via $GW dev br-int"
 ip -6 r a default via $GW dev br-int
-rip -6 r d default via $GW dev $IF
-ovs-vsctl set-controller br-int tcp:$IP_CTLR:6633
+echo "ip -6 r d default via $GW dev $IF"
+ip -6 r d default via $GW dev $IF
+echo "ovs-vsctl set-controller br-int tcp:[$IP_CTLR]:6633"
+ovs-vsctl set-controller br-int tcp:[$IP_CTLR]:6633
 # show status
+echo "ovs-vsctl show"
 ovs-vsctl show
-
+echo "ip l s br-int up"
+ip l s br-int up
